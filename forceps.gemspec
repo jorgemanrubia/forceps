25a603d8e08bba0d23d8a6542eaafc48008f5691
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-02 13:37:59 +0100

    clean test remote database with database cleaner

--- forceps.gemspec
+++ forceps.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 4.0.2"
 
   s.add_development_dependency "sqlite3"
+  s.add_development_dependency "database_cleaner"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require "rails/test_help"
+require 'database_cleaner'
 
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -10,6 +11,11 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+
+DatabaseCleaner[:active_record, connection: :target].strategy = :transaction
+
+class ActiveSupport::TestCase
+  setup { DatabaseCleaner[:active_record, connection: :target].start }
+  teardown { DatabaseCleaner[:active_record, connection: :target].clean }
 end
