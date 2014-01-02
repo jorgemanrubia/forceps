33a414c9a8436382c89b29ca3b90b7681292c8a7
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-02 13:42:32 +0100

    change 'target' by 'remote' to make more clear its meaning

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Load fixtures from the engine
 ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 
-DatabaseCleaner[:active_record, connection: :target].strategy = :transaction
+DatabaseCleaner[:active_record, connection: :remote].strategy = :transaction
 
 class ActiveSupport::TestCase
   setup { DatabaseCleaner[:active_record, connection: :target].start }
