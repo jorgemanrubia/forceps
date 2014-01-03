4575f9834247d37c2a54040cd15f68c9c0ae27c1
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-03 19:35:18 +0100

    Failing test for copying has_many

--- test/acceptance/simple_test.rb
+++ test/acceptance/simple_test.rb
@@ -5,11 +5,21 @@ class SimpleTest < ActiveSupport::TestCase
 
   def setup
     @remote_invoice = RemoteInvoice.create! number: 123, date: Time.now
+    Forceps.configure
   end
 
   test "should download a single record" do
-    Forceps.configure
     Forceps::Remote::Invoice.find_by_number(@remote_invoice.number).copy_to_local
     assert_identical @remote_invoice.becomes(Invoice), Invoice.find_by_number(123)
+  end
+
+  test "should download a record with 'has_many' associated objects" do
+    @remote_user = RemoteUser.create! name: 'Jorge'
+    2.times{|index| @remote_user.invoices.create number: 1, date: "2014-1-#{index}"}
+    Forceps::Remote::User.find(@remote_user.id).copy_to_local
+
+    copied_user = User.find_by_name('Jorge')
+    assert_identical @remote_user, copied_user
+    2.times{|index| assert_identical @remote_user.invoices[index], copied_user.invoices[index]}
   end
 end
