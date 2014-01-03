8511146051e9cbc1b26df88464f8e29e2cc25081
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-03 19:06:29 +0100

    make custom identity assert smarter

--- test/support/minitest_ext.rb
+++ test/support/minitest_ext.rb
@@ -2,6 +2,7 @@ module Minitest
   module Assertions
     # Compares the equality at the attribute level
     def assert_identical expected, actual, msg = nil
+      actual = actual.becomes(expected.class) if actual && actual.class != expected.class
       ignored_attributes = %w(id)
       assert_equal expected.class, actual.class
       assert_equal expected.attributes.except(*ignored_attributes),
