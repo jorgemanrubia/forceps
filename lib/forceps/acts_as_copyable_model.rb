df2d4852d7bc3694437c4c62daa5c8ad7a698b04
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-03 19:36:10 +0100

    extract cloning logic to method object

--- lib/forceps/acts_as_copyable_model.rb
+++ lib/forceps/acts_as_copyable_model.rb
@@ -4,20 +4,29 @@ module Forceps
 
     def copy_to_local
       without_record_timestamps do
-        # 'self.dup.becomes(Invoice)' won't work because of different  AR connections.
-        # todo: prepare for rails 3 and attribute protection
-        cloned_record = self.class.base_class.new(self.attributes.except('id'))
-        cloned_record.save!
+        deep_copier.copy(self)
       end
     end
 
     private
+
+    def deep_copier
+      @deep_copier ||= DeepCopier.new
+    end
 
     def without_record_timestamps
       self.class.base_class.record_timestamps = false
       yield
     ensure
       self.class.base_class.record_timestamps = true
+    end
+
+    class DeepCopier
+      def copy(record)
+        # 'self.dup.becomes(Invoice)' won't work because of different  AR connections.
+        # todo: prepare for rails 3 and attribute protection
+        record.class.base_class.create!(record.attributes.except('id'))
+      end
     end
   end
 end
