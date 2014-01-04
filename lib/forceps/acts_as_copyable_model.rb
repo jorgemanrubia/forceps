6fabd5e01f204977e8a7c8268fab043a72c0f110
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-04 15:32:37 +0100

    extract methods and clarify names

--- lib/forceps/acts_as_copyable_model.rb
+++ lib/forceps/acts_as_copyable_model.rb
@@ -26,14 +26,24 @@ module Forceps
         # 'self.dup.becomes(Invoice)' won't work because of different  AR connections.
         # todo: prepare for rails 3 and attribute protection
         copied_object = remote_object.class.base_class.create!(remote_object.attributes.except('id'))
-
-        remote_object.class.reflect_on_all_associations(:has_many).collect(&:name).each do |association_name|
-          remote_object.send(association_name).find_each do |remote_associated_object|
-            remote_object.send(association_name) << copy(remote_associated_object)
-          end
-        end
-
+        copy_associated_objects(remote_object)
         copied_object
+      end
+
+      def copy_associated_objects(remote_object)
+        copy_objects_associated_by_association_kind(remote_object, :has_many)
+      end
+
+      def copy_objects_associated_by_association_kind(remote_object, association_kind)
+        remote_object.class.reflect_on_all_associations(association_kind).collect(&:name).each do |association_name|
+          copy_associated_objects_by(remote_object, association_name)
+        end
+      end
+
+      def copy_associated_objects_by(remote_object, association_name)
+        remote_object.send(association_name).find_each do |remote_associated_object|
+          remote_object.send(association_name) << copy(remote_associated_object)
+        end
       end
     end
   end
