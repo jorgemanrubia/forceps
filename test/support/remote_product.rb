859d7b093f09b5501e0b8cce90ab6ae8c5bb8b76
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-14 12:36:22 +0100

    Fix testing remote counterparts to reuse proper tables

--- test/support/remote_product.rb
+++ test/support/remote_product.rb
@@ -2,7 +2,7 @@ class RemoteProduct < Product
   establish_connection 'remote'
 
   has_many :line_items, class_name: 'RemoteLineItem'
-  has_and_belongs_to_many :tags, class_name: 'RemoteTag'
+  has_and_belongs_to_many :tags, class_name: 'RemoteTag', join_table: 'products_tags', foreign_key: 'product_id', association_foreign_key: 'tag_id'
 end
 
 
--- test/support/remote_tag.rb
+++ test/support/remote_tag.rb
@@ -2,7 +2,9 @@ class RemoteTag < Product
   establish_connection 'remote'
 
   has_many :line_items, class_name: 'RemoteLineItem'
-  has_and_belongs_to_many :products, class_name: 'RemoteProduct'
+  has_and_belongs_to_many :products, class_name: 'RemoteProduct', join_table: 'products_tags', foreign_key: 'tag_id', association_foreign_key: 'product_id'
+
+
 end
 
 
