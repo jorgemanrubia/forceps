bf7343921ff4ac2535df87a584130fd39fbf1c96
Jorge Manrubia <jorge.manrubia@gmail.com>
2014-01-14 12:25:15 +0100

    Add remote counterparts for testing tags and products

--- test/support/remote_tag.rb
+++ test/support/remote_tag.rb
@@ -1,8 +1,8 @@
-class RemoteProduct < Product
+class RemoteTag < Product
   establish_connection 'remote'
 
   has_many :line_items, class_name: 'RemoteLineItem'
-  has_and_belongs_to_many :tags, class_name: 'RemoteTag'
+  has_and_belongs_to_many :products, class_name: 'RemoteProduct'
 end
 
 
