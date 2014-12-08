6bfd84e02921acd149d56b5752b15fd858735709
Alex Burgel <aburgel@betterment.com>
2014-12-08 00:56:26 -0500

    Fix establish_connection warnings

--- test/support/remote_invoice.rb
+++ test/support/remote_invoice.rb
@@ -1,8 +1,6 @@
 class RemoteInvoice < Invoice
-  establish_connection 'remote'
+  establish_connection :remote
 
   belongs_to :user, class_name: 'RemoteUser'
   has_many :line_items, class_name: 'RemoteLineItem'
 end
-
-
--- test/support/remote_user.rb
+++ test/support/remote_user.rb
@@ -1,8 +1,6 @@
 class RemoteUser < User
-  establish_connection 'remote'
+  establish_connection :remote
 
   has_one :address, class_name: 'RemoteAddress', foreign_key: 'user_id'
   has_many :invoices, class_name: 'RemoteInvoice', foreign_key: 'user_id'
 end
-
-
